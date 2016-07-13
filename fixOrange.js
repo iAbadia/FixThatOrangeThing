b1bade8933079d9f62f06eefaa2292695ae610ef
iabadia <i.abadiaosta@gmail.com>
2016-07-13 23:03:13 +0200

    Fix that orange thing, base

---
+++ fixOrange.js
@@ -0,0 +1,8 @@
+var cells = document.getElementsByClassName('cell');
+var delivery = document.getElementsByClassName('ygtvtableel1');
+for (var i = 0, l = cells.length; i < l; i++) {
+  cells[i].setAttribute('style', 'background-color: #fff; color: #000;');
+}
+for (var i = 0; i < delivery.length; i++) {
+  delivery[i].setAttribute('style', 'background-color: #fff; color: #000;');
+}
